1d402cefc2eabd6fade75601c1c82a0099c965fc
SebastienArthaud <63235835+SebastienArthaud@users.noreply.github.com>
2024-02-02 11:39:18 +0100

    Update main.tf

--- example/main.tf
+++ example/main.tf
@@ -1,5 +1,5 @@
 module "az_windows_virtual_machine" {
-  source                              = "../../az_windows_virtual_machine"
+  source                              = "github.com/SebastienArthaud/az_windows_virtual_machine.git"
   resourcegroup_name                  = var.vm_resourcegroup_name
   virtual_machine_name                = var.virtual_machine_name
   vm_network_interface_name_subnet_id = data.azurerm_subnet.subnet.id
